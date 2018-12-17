8585c3ef9a3c956608eba5ed4a2e88b7f513c2b4
wyant <wyant@AWIYANTO-TY260>
2018-12-17 10:44:29 +0700

    Memulai penggunaan hibernate dan log4j

--- boot182-web/maven-demo/src/main/java/com/xsis/boot182/model/Orang.java
+++ boot182-web/maven-demo/src/main/java/com/xsis/boot182/model/Orang.java
@@ -3,12 +3,36 @@ package com.xsis.boot182.model;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name="T_ORANG")
 public class Orang {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private int id;
+	
+	@Column(name="ORGNM", length=50)
 	private String nama;
+	
+	@Column(name="ORGADDR", length=100)
 	private String alamat;
+	
+	@Column(name="ORGJK", length=1)
 	private String jk;
+	
+	@Temporal(TemporalType.DATE)
+	@Column(name="ORGDOB")
 	private Date tglLahir;
+	
+	@Column(name="ORGPOB", length=50)
 	private String tmpLahir;
 	
 	public Orang() {
